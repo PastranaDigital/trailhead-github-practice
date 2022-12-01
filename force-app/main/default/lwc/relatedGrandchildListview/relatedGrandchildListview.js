6b2690a3bf079c50b156e1ab4437ad8792d9c420
Omar Pastrana <Omar.Pastrana@gmail.com>
2022-12-01 17:42:36 -0600

    updated css and html layout

--- force-app/main/default/lwc/relatedGrandchildListview/relatedGrandchildListview.js
+++ force-app/main/default/lwc/relatedGrandchildListview/relatedGrandchildListview.js
@@ -1,11 +1,14 @@
 import { LightningElement, api } from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import getRelatedRecords from '@salesforce/apex/RelatedGCListviewController.getRelated';
+import getRelatedRecords from '@salesforce/apex/RelatedGCListviewController.getRelatedAddendums';
 
 export default class RelatedGrandchildListview extends LightningElement {
 	@api recordId;
+	limitForRecords = 3;
 	records;
 	error;
+	componentTitle = 'Addendums (0)';
+	URL = '/';
 
 	connectedCallback() {
 		this.getRecords();
@@ -15,13 +18,24 @@ export default class RelatedGrandchildListview extends LightningElement {
 		getRelatedRecords ({ incomingId: this.recordId })
 			.then((result) => {
 				this.records = result;
+				let updatedRecords = [];
+				this.records.forEach(record => {
+					let recordData = record;
+					//? modifications to the object
+					recordData.addendumURL = this.URL + record.Id;
+					recordData.parentURL = this.URL + record.Note__c;
+
+					updatedRecords.push(recordData);
+				});
+				this.records = updatedRecords.slice(0, this.limitForRecords);
+				this.componentTitle = this.componentTitle.replace('0', updatedRecords.length);
 				this.error = undefined;
 				console.log('this.records: ', JSON.parse(JSON.stringify(this.records)));
 			})
 			.catch((error) => {
 				this.error = error;
 				this.records = undefined;
-				console.log('error', this.error);
+				console.error('ERROR: ', this.error.message);
 			});
 	}
 }
